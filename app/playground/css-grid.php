f9a8242ac40c7d6729d62fa1c336a475e09bc8f7
Ralph <ralpgarc@cisco.com>
2018-05-24 17:23:35 -0700

    updates

--- app/playground/css-grid.php
+++ app/playground/css-grid.php
@@ -4,15 +4,21 @@
     $ROOT = '../'; // Set root for relative paths
     $thisPage="css-grid"; 
 ?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<head>
-<title><?php echo hmpgTitle ?></title>
-<?php include("../inc/head.inc.php"); ?>
-</head>
+    <!DOCTYPE html>
+    <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <html class="no-js">
+    <!--<![endif]-->
+
+    <head>
+        <title>
+            <?php echo hmpgTitle ?>
+        </title>
+        <?php include("../inc/head.inc.php"); ?>
+    </head>
+
     <body class="second-level">
         <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -20,26 +26,29 @@
         <main class="chef-grid">
             <?php include("../inc/nav.inc.php"); ?>
             <section class="chef-grid--content">
-              
-                <section>
-                    <h1>CSS Grid</h1>
-                    <p>
-                        <a href="https://css-tricks.com/snippets/css/complete-guide-grid/#prop-display">CSS Tricks: css-grid</a>
-                    </p>
-                    <p>
-                        <a href="holy-grail-grid.php">Holy Grail CSS Grid</a>
-                    </p>
-                    <div class="css-grid css-grid--col-3">
-                        <div class="css-grid--col">1
-                        <p>xx</p><p>xx</p><p>xx</p><p>xx</p><p>xx</p><p>xx</p><p>xx</p>
-                        </div>
-                        <div class="css-grid--col">2</div>
-                        <div class="css-grid--col">3</div>
+                <h1>CSS Grid</h1>
+                <p>
+                    <a href="https://css-tricks.com/snippets/css/complete-guide-grid/#prop-display">CSS Tricks: css-grid</a>
+                </p>
+                <p>
+                    <a href="holy-grail-grid.php">Holy Grail CSS Grid</a>
+                </p>
+                <div class="css-grid css-grid--col-3">
+                    <div class="css-grid--col">1
+                        <p>xx</p>
+                        <p>xx</p>
+                        <p>xx</p>
+                        <p>xx</p>
+                        <p>xx</p>
+                        <p>xx</p>
+                        <p>xx</p>
                     </div>
-                </section>
-          
+                    <div class="css-grid--col">2</div>
+                    <div class="css-grid--col">3</div>
+                </div>
             </section>
             <?php include("../inc/foot.inc.php"); ?>
         </main>
     </body>
-</html>
+
+    </html>
